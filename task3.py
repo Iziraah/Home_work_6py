efc682f6fb9738f60ca803a78f108f5bb7e0980a
Mariia <>
2022-07-31 16:36:24 +0300

    сумма элементов на нечетной позиции2

--- task3.py
+++ task3.py
@@ -1,5 +1,5 @@
 #Найти сумму чисел списка стоящих на нечетной позиции
 
-subsequence = list(map(int,input('Введите значения цен через пробел ').split()))
-b = subsequence[1::2]
+arr = list(map(int,input('Введите последовательность чисел через пробел ').split()))
+b = arr[0::2]
 print(sum(b))
